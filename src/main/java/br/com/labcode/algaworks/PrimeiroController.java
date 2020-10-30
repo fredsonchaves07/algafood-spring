8edc670ae9b7186a60dd5239ee63ab9755609087
Fredson Chaves <fredsonchaves07@gmail.com>
2020-10-30 19:22:52 -0300

    first commit

---
+++ src/main/java/br/com/labcode/algaworks/PrimeiroController.java
@@ -0,0 +1,15 @@
+package br.com.labcode.algaworks;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PrimeiroController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWord(){
+        return "Hello!";
+    }
+}
---
+++ src/test/java/br/com/labcode/algaworks/AlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.labcode.algaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
