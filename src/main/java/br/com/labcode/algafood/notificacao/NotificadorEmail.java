03fcd5059a572c6ef6ea3f08d444355c8f30d8fe
Fredson Chaves <fredsonchaves07@gmail.com>
2020-11-16 18:58:57 -0300

    pontos de injeção de dependencia com autowired

--- src/main/java/br/com/labcode/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/br/com/labcode/algafood/notificacao/NotificadorEmail.java
@@ -5,28 +5,11 @@ import br.com.labcode.algafood.service.AtivacaoClienteService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-
+@Component
 public class NotificadorEmail implements Notificador {
-    private boolean caixaAlta;
-    private String hostServidorSmtp;
 
-    public NotificadorEmail(String hostServidorSmtp){
-        this.hostServidorSmtp = hostServidorSmtp;
-        System.out.println("NotificadorEmail");
+    public void setCaixaAlta() {
+
     }
-
-    @Override
-    public void notificar(Cliente cliente, String mensagem){
-        if(caixaAlta){
-            mensagem = mensagem.toUpperCase();
-        }
-
-        System.out.printf("Notificando %s através do email %s: %s", cliente.getNome(), cliente.getEmail(), mensagem);
-    }
-
-    public void setCaixaAlta(boolean caixaAlta) {
-        this.caixaAlta = caixaAlta;
-    }
-
 
 }
--- src/main/java/br/com/labcode/algafood/service/AtivacaoClienteService.java
+++ src/main/java/br/com/labcode/algafood/service/AtivacaoClienteService.java
@@ -3,22 +3,33 @@ package br.com.labcode.algafood.service;
 import br.com.labcode.algafood.modelo.Cliente;
 import br.com.labcode.algafood.notificacao.Notificador;
 import br.com.labcode.algafood.notificacao.NotificadorEmail;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 public class AtivacaoClienteService {
 
+    // Também é possível injetar através do atributo
+    // Problema é dificuldades com testes
+    @Autowired
     private Notificador notificador;
-
+    // Uso dessa anotação defin um ponto de injeção
+    // A anotação fica opcional quando tem apenas 1 construtor
+    // Ponto de injeção por construtor
+    // O ideal é deixar no construtor
+    @Autowired
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
-
-        System.out.println("AtivacaoClienteService: " + notificador);
     }
 
     public void ativar(Cliente cliente){
         cliente.ativarCliente();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+    }
+    // Também é possível atribuit um ponto de injeção no método com métodos
+    @Autowired
+    public void setNotificador(Notificador notificador) {
+        this.notificador = notificador;
     }
 }
